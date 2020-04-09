54404009b9046b355874d8d575463ce18ecb3751
chrisallo <chrisallo.h@gmail.com>
2020-04-10 04:13:05 +0900

    add more tests

--- test/unit/kernel/lib/transaction.js
+++ test/unit/kernel/lib/transaction.js
@@ -34,5 +34,36 @@ export default function () {
       assert.isTrue(backup === null || backup === '[]');
       assert.equal(data, 'test data');
     });
+    it('addJob x3 > flush', async function () {
+      transaction.addJob(EsperTransaction.Action.PUT, 'key1', 'test data 1');
+      transaction.addJob(EsperTransaction.Action.PUT, 'key2', 'test data 2');
+      transaction.addJob(EsperTransaction.Action.REMOVE, 'key1');
+      await transaction.flush();
+
+      const backup = JSON.parse(await store.getItem(transaction.backupKey));
+      const data1 = JSON.parse(await store.getItem('key1'));
+      const data2 = JSON.parse(await store.getItem('key2'));
+      assert.isTrue(backup === null || backup === '[]');
+      assert.isNull(data1);
+      assert.equal(data2, 'test data 2');
+    });
+    it('addJob x2 > flush > addJob x2 > flush', async function () {
+      transaction.addJob(EsperTransaction.Action.PUT, 'key1', 'test data 1');
+      transaction.addJob(EsperTransaction.Action.PUT, 'key2', 'test data 2');
+      await transaction.flush();
+
+      transaction.addJob(EsperTransaction.Action.PUT, 'key3', 'test data 3');
+      transaction.addJob(EsperTransaction.Action.REMOVE, 'key1');
+      await transaction.flush();
+
+      const backup = JSON.parse(await store.getItem(transaction.backupKey));
+      const data1 = JSON.parse(await store.getItem('key1'));
+      const data2 = JSON.parse(await store.getItem('key2'));
+      const data3 = JSON.parse(await store.getItem('key3'));
+      assert.isTrue(backup === null || backup === '[]');
+      assert.isNull(data1);
+      assert.equal(data2, 'test data 2');
+      assert.equal(data3, 'test data 3');
+    });
   });
 };
